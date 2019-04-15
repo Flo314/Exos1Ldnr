bef01769bc67a6313e3c459a2b6d116402fabc02
“Florent <f.losada@hotmail.fr>
2019-04-15 14:39:44 +0200

    clicked login intent listview ok

--- app/src/main/java/com/example/exos1ldnr/ListActivity.java
+++ app/src/main/java/com/example/exos1ldnr/ListActivity.java
@@ -2,12 +2,29 @@ package com.example.exos1ldnr;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class ListActivity extends AppCompatActivity {
 
+    private ListView myListView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
+
+        // récupération de la listView
+         myListView = findViewById(R.id.myListView);
+
+        // intanciation d'un adapteur avec ma liste et mon layout et l'id du textView
+        ArrayAdapter<String> tableau = new ArrayAdapter<>(myListView.getContext(),R.layout.listview_elements,R.id.textMyListView);
+
+        // on ajoute nos chaînes de caractère
+        for(int i = 1; i<41; i++){
+            tableau.add("Item n°" + i);
+        }
+
+        // on affiche cet adapteur
+        myListView.setAdapter(tableau);
     }
 }
--- app/src/main/java/com/example/exos1ldnr/Login.java
+++ app/src/main/java/com/example/exos1ldnr/Login.java
@@ -1,13 +1,33 @@
 package com.example.exos1ldnr;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class Login extends AppCompatActivity {
+
+    private EditText username;
+    private EditText password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        username = (EditText) findViewById(R.id.username);
+        password = (EditText) findViewById(R.id.password);
+
+    }
+
+    protected void loginButtonClicked(View view){
+        Toast.makeText(this, username.getText() + " " + password.getText() + " : You clicked !", Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(this, ListActivity.class);
+        startActivity(intent);
     }
 }
